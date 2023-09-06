f87614dbfbf2793267bfcfcf79b8afb9c382d420
ozaki <koki.ozaki98@gmail.com>
2023-09-06 16:06:13 +0900

    コミット

--- js/index.js
+++ js/index.js
@@ -7,7 +7,10 @@ function calculateTotalPrice() {
   let selectFan = document.querySelector('input[name="fan"]:checked');
   let selectGrease = document.querySelector('input[name="grease"]:checked');
   let selectCutpro = document.querySelector('input[name="cutpro"]:checked');
-  let selectGrapchic = document.querySelector('input[name="graphic"]:checked');
+  let selectGraphic = document.querySelector('input[name="graphic"]:checked');
+  let selectPower = document.querySelector('input[name="power"]:checked');
+  let selectMemory = document.querySelector('input[name="memory"]:checked');
+  let selectSsd = document.querySelector('input[name="ssd"]:checked');
   
   // os選択
   if (selectOs) {
@@ -45,8 +48,8 @@ function calculateTotalPrice() {
   }
 
   // グラフィック選択
-  if (selectGrapchic) {
-    let price = parseInt(selectGrapchic.dataset.price);
+  if (selectGraphic) {
+    let price = parseInt(selectGraphic.dataset.price);
     totalPrice += price;
   }
 
@@ -55,22 +58,88 @@ function calculateTotalPrice() {
     let price = parseInt(selectCutpro.dataset.price);
     totalPrice += price;
   }
+  // 電源選択
+  if (selectPower) {
+    let price = parseInt(selectPower.dataset.price);
+    totalPrice += price;
+  }
+  // メモリ選択
+  if (selectMemory) {
+    let price = parseInt(selectMemory.dataset.price);
+    totalPrice += price;
+  }
+  // SSD選択
+  if (selectSsd) {
+    let price = parseInt(selectSsd.dataset.price);
+    totalPrice += price;
+  }
   
   document.querySelector('.total p span').textContent = `${totalPrice}円`;
+
+  return totalPrice;
 }
 
 function confirmShow() {
-  const inputOs = document.querySelectorAll('input[name="os"]');
+  let selectOs = document.querySelector('input[name="os"]:checked + label');
+  let selectOffice = document.querySelector('input[name="office"]:checked + label');
+  let selectSecurity = document.querySelector('input[name="security"]:checked + label');
+  let selectCpu = document.querySelector('input[name="cpu"]:checked + label');
+  let selectFan = document.querySelector('input[name="fan"]:checked + label');
+  let selectGrease = document.querySelector('input[name="grease"]:checked + label');
+  let selectCutpro = document.querySelector('input[name="cutpro"]:checked + label');
+  let selectGraphic = document.querySelector('input[name="graphic"]:checked + label');
+  let selectPower = document.querySelector('input[name="power"]:checked + label');
+  let selectMemory = document.querySelector('input[name="memory"]:checked + label');
+  let selectSsd = document.querySelector('input[name="ssd"]:checked + label');
 
-  const selectOs = document.querySelector('.select-os');
+  let confirmContents = document.querySelector('.confirm__contents');
 
-  inputOs.forEach((input) => {
-    input.addEventListener('click', () => {
-      const selectValue = input.value;
+  confirmContents.innerHTML = `
+    <h3 class="confirm__ttl">カスタマイズ内容の確認</h3>
+    <dl>
+      <dt>OS</dt>
+      <dd class="select-os">${selectOs.textContent}</dd>
+    </dl>
+    <dl>
+      <dt>オフィスソフト</dt>
+      <dd>${selectOffice.textContent}</dd>
+    </dl>
+    <dl>
+      <dt>セキュリティソフト</dt>
+      <dd>${selectSecurity.textContent}</dd>
+    </dl>
+    <dl>
+      <dt>CPUファン</dt>
+      <dd>${selectFan.textContent}</dd>
+    </dl>
+    <dl>
+      <dt>CPUグリス</dt>
+      <dd>${selectGrease.textContent}</dd>
+    </dl>
+    <dl>
+      <dt>グラフィック機能</dt>
+      <dd>${selectGraphic.textContent}</dd>
+    </dl>
+    <dl>
+      <dt>電源</dt>
+      <dd>${selectPower.textContent}</dd>
+    </dl>
+    <dl>
+      <dt>メモリ</dt>
+      <dd>${selectMemory.textContent}</dd>
+    </dl>
+    <dl>
+      <dt>SSD</dt>
+      <dd>${selectSsd.textContent}</dd>
+    </dl>
+    <p class="total-price">合計金額:<span></span></p>
+  `;
+  let totalPriceElement = document.querySelector('.total-price span');
+  let totalPrice = calculateTotalPrice();
+  totalPriceElement.textContent = `${totalPrice}円`;
 
-      selectOs.textContent = selectValue;
-    })
-  }) 
+  let confirmSection = document.querySelector('.confirm');
+  confirmSection.style.display = 'block';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -130,13 +199,21 @@ cutproOptions.forEach(function(option) {
   option.addEventListener('change', calculateTotalPrice);
 })
 
+let powerOptions = document.querySelectorAll('input[name="power"]');
+powerOptions.forEach(function(option) {
+  option.addEventListener('change', calculateTotalPrice);
+})
+let memoryOptions = document.querySelectorAll('input[name="memory"]');
+memoryOptions.forEach(function(option) {
+  option.addEventListener('change', calculateTotalPrice);
+})
+let memorySsd = document.querySelectorAll('input[name="ssd"]');
+memorySsd.forEach(function(option) {
+  option.addEventListener('change', calculateTotalPrice);
+})
 
 const orderBtn = document.querySelector('.order-btn');
-const confirm = document.querySelector('.confirm');
-orderBtn.addEventListener('click', () => {
-  confirm.style.display = "block";
-});
-
+orderBtn.addEventListener('click', confirmShow);
 
 // ページ読み込み時に計算を実行
 calculateTotalPrice();
