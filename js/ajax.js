5e3df74e24188261ea75def140706faff4827af7
ozaki98 <k.ozaki@insel-art.jp>
2023-09-13 13:30:02 +0900

    ajax読み込み方変更

--- js/ajax.js
+++ js/ajax.js
@@ -4,7 +4,48 @@ $(document).ready(function() {
   $.ajax({
     url: "../data.json",
     dataType: "json",
-    success: function(optionsData) {
+    // success: function(optionsData) {
+    //   console.log(optionsData);
+
+    //   const optionNames = [
+    //     'os', 'office', 'security', 'cpu', 'fan', 'grease', 'graphic', 'power', 'memory',
+    //     'ssd', 'harddisk', 'addhard', 'drive', 'sound', 'case', 'rear', 'topfront',
+    //     'toprear', 'lan', 'wireless'
+    //   ];
+
+    //   // 共通の関数を作成してオプションを生成
+    //   optionNames.forEach(optionName => {
+    //     generateOptions(optionsData[`${optionName}Options`], optionName, optionName);
+    //   });
+
+    //   // 価格反映
+    //   optionNames.forEach(optionName => {
+    //     $(`input[name="${optionName}"]`).on('change', calculateTotalPrice);
+    //   });
+
+    //   // タブ切り替え
+    //   const tabs = $('.product__item');
+    //   tabs.on('click', tabSwitch);
+
+    //   function tabSwitch() {
+    //     $('.is-active').removeClass('is-active');
+    //     $(this).addClass('is-active');
+
+    //     $('.is-show').removeClass('is-show');
+    //     const index = tabs.index(this);
+    //     $('.product').eq(index).addClass('is-show');
+    //   }
+
+    //   // ページ読み込み時に計算を実行
+    //   calculateTotalPrice();
+    // },
+    // error: function(error) {
+    //   console.error("データを取得できませんでした: " + error);
+    // }
+  })
+  .then( 
+    // 成功したとき
+    function(optionsData) {
       console.log(optionsData);
 
       const optionNames = [
@@ -39,10 +80,13 @@ $(document).ready(function() {
       // ページ読み込み時に計算を実行
       calculateTotalPrice();
     },
-    error: function(error) {
+  
+    // エラーの時
+    function () {
       console.error("データを取得できませんでした: " + error);
+
     }
-  });
+    );
 
 
   function generateOptions(optionData, sectionClassName, inputName) {
